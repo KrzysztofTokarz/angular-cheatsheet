03839f6d9cb084d133a19d8e6214eba84e73f3a4
Krzysztof Tokarz <k.tokarz@andersenlab.com>
2023-06-16 16:22:23 +0200

    complete signals example

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [CommonModule, MatInputModule, FormsModule],
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InputComponent), multi: true }],
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label = ''
+  @Input() placeholder = ''
+
+  private onChange = (value: string) => {};
+  private onTouch = (value: string) => {};
+  protected _value = '';
+
+  onInput(value: string) {
+    this._value = value;
+    this.onChange(value);
+    this.onTouch(value);
+  }
+
+  writeValue(value: string) {
+    this._value = value;
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouch = fn;
+  }
+}
--- src/app/features/angular16/signals/signals.component.ts
+++ src/app/features/angular16/signals/signals.component.ts
@@ -1,12 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { InputComponent } from '../../../components/input/input.component';
 
 @Component({
   selector: 'app-signals',
   standalone: true,
   templateUrl: './signals.component.html',
   styleUrls: ['./signals.component.scss'],
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, InputComponent, FormsModule],
 })
-export class SignalsComponent {}
+export class SignalsComponent {
+  counter = signal(0);
+  text = signal('Some text');
+  doubleCounter = computed(() => this.counter() * 2);
+  staticCounter = this.counter() * 2;
+  textFromInputAndCounter = computed(() => `${this.text()} + ${this.counter()}`);
+
+  constructor() {
+    effect(() => {
+      console.log(`I will run when counter changes - ${this.counter()}`);
+    });
+    effect(() => {
+      console.log(`I will run when text changes - ${this.text()}`);
+    });
+    effect(() => {
+      console.log(`I will run when counter or text changes - ${this.text()} + ${this.counter()}`);
+    });
+  }
+
+  increment() {
+    this.counter.update((x) => x + 1);
+  }
+}
